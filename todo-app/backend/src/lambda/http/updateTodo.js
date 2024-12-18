d4ee67eb5579dadf0e1c658efc9316f9beb9bd84
Michael Ly <17331561+codeaftermath@users.noreply.github.com>
2024-12-18 14:49:34 -0800

    Add header to allow credentials

--- todo-app/backend/src/lambda/http/updateTodo.js
+++ todo-app/backend/src/lambda/http/updateTodo.js
@@ -10,7 +10,8 @@ export async function handler(event) {
   return {
     statusCode: 201,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
     },
     body: JSON.stringify({
       item: result
