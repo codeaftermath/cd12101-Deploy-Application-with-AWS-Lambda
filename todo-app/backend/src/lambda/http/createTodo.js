b2c77421a8fcd60d5cdcd1b1392d6915073fe8ca
Michael Ly <17331561+codeaftermath@users.noreply.github.com>
2024-12-18 17:23:43 -0800

    Refactor create todo handler to use middy and http-errors

--- todo-app/backend/src/lambda/http/createTodo.js
+++ todo-app/backend/src/lambda/http/createTodo.js
@@ -1,25 +1,55 @@
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
+
 import * as todoService from '../../service/todoService.js'
 import { getUserId } from '../utils.mjs'
 import { createLogger } from '../../utils/logger.mjs'
 
 const logger = createLogger('createTodoHandler')
 
-export async function handler(event) {
-  const userId = getUserId(event)
-  const newTodo = JSON.parse(event.body)
-  logger.info('Creating todo', {
-    userId,
-    newTodo
-  })
-  const result = await todoService.createTodo(userId, newTodo)
-  return {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
-    },
-    body: JSON.stringify({
-      item: result
+const lambdaHandler = async (event) => {
+  try {
+    const userId = getUserId(event)
+    const newTodo = JSON.parse(event.body)
+    logger.info('Creating todo', {
+      userId,
+      newTodo
     })
+
+    const result = await todoService.createTodo(userId, newTodo)
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        item: result
+      })
+    }
+  } catch (err) {
+    logger.error('Error Creating Todo', {
+      event: {
+        body: event.body,
+        pathParameters: event.pathParameters,
+        queryStringParameters: event.queryStringParameters,
+        path: event.path
+      },
+      error: err
+    })
+    throw createError(
+      500,
+      JSON.stringify({
+        error: 'Server error creating todo'
+      })
+    )
   }
 }
+
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
+  .handler(lambdaHandler)
