62f2dbd6c7dc48a8497ab73ed5a3ae5d99b766f3
Michael Ly <17331561+codeaftermath@users.noreply.github.com>
2024-12-18 21:00:00 -0800

    Implement handler for generating upload url lambda

--- todo-app/backend/src/lambda/http/generateUploadUrl.js
+++ todo-app/backend/src/lambda/http/generateUploadUrl.js
@@ -1,7 +1,49 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+import { getUploadUrl } from '../../service/generateUploadUrlService.js'
+import { createLogger } from '../../utils/logger.mjs'
+
+const logger = createLogger('generateUploadUrlHandler')
+
+const lambdaHandler = async (event) => {
+  try {
+    const todoId = event.pathParameters.todoId
+    const filename = JSON.parse(event.body).filename
+    logger.info('Generating upload url', { todoId, filename, event })
+    const uploadUrl = await getUploadUrl(`${todoId}/${filename}`)
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        uploadUrl
+      })
+    }
+  } catch (err) {
+    logger.error('Error generating presigned url', {
+      event: {
+        body: event.body,
+        pathParameters: event.pathParameters,
+        queryStringParameters: event.queryStringParameters,
+        path: event.path
+      },
+      error: err
+    })
+    throw createError(
+      500,
+      JSON.stringify({
+        error: 'Unable to generate upload url'
+      })
+    )
+  }
 }
 
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
+  .handler(lambdaHandler)
