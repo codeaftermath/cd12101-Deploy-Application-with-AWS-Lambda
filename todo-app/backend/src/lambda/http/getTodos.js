bea75e75c29f18d6f3d4e7a741cc44f04e90bdcf
Michael Ly <17331561+codeaftermath@users.noreply.github.com>
2024-12-18 17:39:54 -0800

    Refactoring to include use of middy and http-errors in lambda handlers

--- todo-app/backend/src/lambda/http/getTodos.js
+++ todo-app/backend/src/lambda/http/getTodos.js
@@ -1,21 +1,51 @@
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
+
 import * as todoService from '../../service/todoService.js'
 import { getUserId } from '../utils.mjs'
 import { createLogger } from '../../utils/logger.mjs'
 
 const logger = createLogger('getTodoHandler')
 
-export async function handler(event) {
-  const userId = getUserId(event)
-  logger.info('Fetching todos', { userId })
-  const result = await todoService.getAllTodos(userId)
-  return {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
-    },
-    body: JSON.stringify({
-      items: result
+const lambdaHandler = async (event) => {
+  try {
+    const userId = getUserId(event)
+    logger.info('Fetching todos', { userId })
+
+    const result = await todoService.getAllTodos(userId)
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        items: result
+      })
+    }
+  } catch (err) {
+    logger.error('Error Getting Todo(s)', {
+      event: {
+        body: event.body,
+        pathParameters: event.pathParameters,
+        queryStringParameters: event.queryStringParameters,
+        path: event.path
+      },
+      error: err
     })
+    throw createError(
+      500,
+      JSON.stringify({
+        error: 'Server error getting todo(s)'
+      })
+    )
   }
 }
+
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
+  .handler(lambdaHandler)
